36cab14fb5760145385a8d93715d2c3418568353
Kyle Begeman <kyle.begeman@leafly.com>
2019-01-09 14:53:33 -0800

    Initial Commit

---
+++ Extensions/Extensions.h
@@ -0,0 +1,19 @@
+//
+//  Extensions.h
+//  Extensions
+//
+//  Created by Kyle Begeman on 1/9/19.
+//  Copyright © 2019 Kyle Begeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Extensions.
+FOUNDATION_EXPORT double ExtensionsVersionNumber;
+
+//! Project version string for Extensions.
+FOUNDATION_EXPORT const unsigned char ExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extensions/PublicHeader.h>
+
+
